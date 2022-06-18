07893bd2144c54f30721fd2b426708b764f3b3a6
Erika <erikamondes@gmail.com>
2022-06-18 02:02:17 -0300

    master

--- App.js
+++ App.js
@@ -1,48 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 import Torch from 'react-native-torch';
 import RNShake from 'react-native-shake';
 
-const App = () => {
+const App: React.FC = () => {
   const [toggle, setToggle] = useState(false);
 
-  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
-
   useEffect(() => {
-    // Liga flash do celular
     Torch.switchState(toggle);
   }, [toggle]);
 
   useEffect(() => {
-    /**
-     * Quando o celular for chacoalhado, mudaremos o toggle
-     */
     const subscription = RNShake.addListener(() => {
-      setToggle(oldToggle => !oldToggle);
+      setToggle(old => !old);
     });
 
-    // Essa func vai ser chamada quando o componets
-    // For ser desmontado
     return () => subscription.remove();
   }, []);
 
   return (
-    <View style={toggle ? style.containerLight : style.container}>
-      <TouchableOpacity onPress={handleChangeToggle}>
+    <View style={style.container}>
+      <TouchableOpacity onPress={() => setToggle(old => !old)}>
         <Image
           style={toggle ? style.lightingOn : style.lightingOff}
           source={
             toggle
               ? require('./assets/icons/eco-light.png')
               : require('./assets/icons/eco-light-off.png')
-          }
-        />
-        <Image
-          style={style.dioLogo}
-          source={
-            toggle
-              ? require('./assets/icons/logo-dio.png')
-              : require('./assets/icons/logo-dio-white.png')
           }
         />
       </TouchableOpacity>
@@ -54,34 +38,20 @@ export default App;
 
 const style = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  containerLight: {
     flex: 1,
-    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   lightingOn: {
     resizeMode: 'contain',
-    alignSelf: 'center',
-    width: 150,
-    height: 150,
+    width: 200,
+    height: 200,
   },
   lightingOff: {
-    resizeMode: 'contain',
-    alignSelf: 'center',
     tintColor: 'white',
-    width: 150,
-    height: 150,
-  },
-  dioLogo: {
     resizeMode: 'contain',
-    alignSelf: 'center',
-    width: 250,
-    height: 250,
+    width: 200,
+    height: 200,
   },
 });
